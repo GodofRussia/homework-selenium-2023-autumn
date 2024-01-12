fee99ff39abf9c41f6264a96e7d263bf990603b9
KolobokMysnoy <Inkasator200@ya.ru>
2024-01-12 23:51:42 +0300

    Check with pycodestyle

--- hw/code/ui/pages/base_page.py
+++ hw/code/ui/pages/base_page.py
@@ -223,7 +223,8 @@ class BasePage(object):
         actions.perform()
         return self
 
-    def action_click_not_clickable(self, element, timeout=WaitTime.MEDIUM_WAIT):
+    def action_click_not_clickable(self, element,
+                                   timeout=WaitTime.MEDIUM_WAIT):
         self.driver.execute_script(
             "arguments[0].scrollIntoView(true);", element
         )
@@ -233,13 +234,16 @@ class BasePage(object):
         actions.perform()
         return self
 
-    def search_action_click(self, locator, what_choose: int = 0, timeout: int = WaitTime.LONG_WAIT):
+    def search_action_click(self, locator, what_choose: int = 0,
+                            timeout: int = WaitTime.LONG_WAIT):
         el = self.multiple_find(locator, timeout)[what_choose]
         self.action_click(el, timeout)
 
         return self
 
-    def search_action_click_not_clickable(self, locator, what_choose: int = 0, timeout: int = WaitTime.LONG_WAIT):
+    def search_action_click_not_clickable(self, locator,
+                                          what_choose: int = 0,
+                                          timeout: int = WaitTime.LONG_WAIT):
         if not timeout:
             timeout = WaitTime.LONG_WAIT
         el = self.multiple_find(locator, timeout)[what_choose]
